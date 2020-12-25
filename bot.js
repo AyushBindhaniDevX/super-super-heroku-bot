68418addf249f8e39f09b47450951f9b448137ea
Ayush Bindhani <73215402+AyushBindhaniDevX@users.noreply.github.com>
2020-12-25 11:57:58 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,14 +9,6 @@ client.on('message', message => {
 if (msg.content === 'ping') {
   msg.reply('pong');
   msg.channel.send('pong');
-
-} else if (msg.content.startsWith('!kick')) {
-  if (msg.mentions.users.size) {
-    const taggedUser = msg.mentions.users.first();
-    msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-  } else {
-    msg.reply('Please tag a valid user!');
-  }
 }
 });
 
