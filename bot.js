59e8cf96145265f390314c2be6b7eec513273f59
Ayush Bindhani <73215402+AyushBindhaniDevX@users.noreply.github.com>
2020-12-25 12:04:02 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,10 @@ client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
+ if (message.content === "?random") { // checks if the message says "?random"
+        const number = Math.random(); // generates a random number
+        message.channel.send(number.toString()); // sends a message to the channel with the number
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
